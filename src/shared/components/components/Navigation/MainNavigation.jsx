ef4abb2fa57d76174ab3e6e2d23b1a7ba599e3c5
Taylor Covington <tcovingtondev@gmail.com>
2019-12-24 00:47:17 -0700

    finished adding animation to mobile nav drawer and onClick funtions

--- src/shared/components/components/Navigation/MainNavigation.jsx
+++ src/shared/components/components/Navigation/MainNavigation.jsx
@@ -1,21 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Link, NavLink } from 'react-router-dom';
 
 import MainHeader from './MainHeader';
 import NavLinks from './NavLinks';
 import './MainNavigation.css';
 import SideDrawer from './SideDrawer';
+import Backdrop from '../UIElements/Backdrop';
 
 const MainNavigation = props => {
+  const [drawerIsOpen, setDrawerIsOpen] = useState(false);
+
+  const openDrawerHandler = () => {
+    setDrawerIsOpen(true);
+  }
+  const closeDrawerHandler = () => {
+    setDrawerIsOpen(false);
+  }
+
   return(
     <>
-    <SideDrawer>
+{drawerIsOpen && <Backdrop onClick={closeDrawerHandler}/> }
+    <SideDrawer show={drawerIsOpen} onClick={closeDrawerHandler}>
       <nav className='main-navigation__drawer-nav'>
         <NavLinks />
       </nav>
     </SideDrawer>
     <MainHeader>
-      <button className='main-navigation__menu-btn'>
+      <button className='main-navigation__menu-btn' onClick={openDrawerHandler}>
         <span />
         <span />
         <span />
